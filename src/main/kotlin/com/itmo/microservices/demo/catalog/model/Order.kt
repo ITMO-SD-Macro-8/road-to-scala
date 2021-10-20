e39f16dfe1ef05b1205f31e52740b1a836ec97d3
Ilia <i.ilmenskiy@corp.vk.com>
2021-10-20 21:16:34 +0300

    added order entity

--- src/main/kotlin/com/itmo/microservices/demo/catalog/model/Order.kt
+++ src/main/kotlin/com/itmo/microservices/demo/catalog/model/Order.kt
@@ -6,9 +6,17 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
-data class Order(
+data class OrderModel(
     val orderId: UUID = UUID.randomUUID(),
     val user: AppUser = AppUser(),
     val date: Date = Date(),
     val positions: List<Position> = emptyList()
+)
+
+@Entity
+data class Order(
+    @Id @GeneratedValue val id: Int = -1,
+    val orderId: UUID = UUID.randomUUID(),
+    val userId: UUID = UUID.randomUUID(),
+    val date: Date = Date()
 )
