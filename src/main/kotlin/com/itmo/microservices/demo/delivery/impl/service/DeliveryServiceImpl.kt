2e5eea473a9e81b24c017c0fd3ece511c31d3895
triyul22 <triyul22@niuitmo.ru>
2021-10-27 22:08:34 +0300

    add delivery service impl

---
+++ src/main/kotlin/com/itmo/microservices/demo/delivery/impl/service/DeliveryServiceImpl.kt
@@ -0,0 +1,27 @@
+package com.itmo.microservices.demo.delivery.impl.service
+
+import com.itmo.microservices.demo.delivery.impl.entity.TimeSlot
+import com.itmo.microservices.demo.order.api.model.OrderModel
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class DeliveryServiceImpl @Autowired constructor()
+    :DeliveryService{
+
+    override fun getNearestTimeSlot(preferredTimeSlot: Date): TimeSlot? {
+        return TimeSlot(UUID.randomUUID(), preferredTimeSlot, preferredTimeSlot, 0, 1);
+    }
+
+    override fun reserveTimeSlot(order: OrderModel, timeSlot: TimeSlot): Boolean {
+        if (timeSlot.reservedCount != timeSlot.reservedMax){
+            timeSlot.reservedCount = timeSlot.reservedCount!! + 1;
+
+            return true;
+        }
+
+        return false;
+    }
+
+}
