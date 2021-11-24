62ff8dfc2689329f5152e32e24c338862e134cc1
i.ilmenskiy <i.ilmenskiy@corp.vk.com>
2021-11-24 19:43:08 +0300

    upd basket

--- src/main/kotlin/com/itmo/microservices/demo/basket/api/controller/BasketController.kt
+++ src/main/kotlin/com/itmo/microservices/demo/basket/api/controller/BasketController.kt
@@ -3,19 +3,29 @@ package com.itmo.microservices.demo.basket.api.controller
 import com.itmo.microservices.demo.basket.api.model.AddCatalogItemToBasket
 import com.itmo.microservices.demo.basket.api.service.BasketService
 import com.itmo.microservices.demo.basket.impl.entity.BookingDto
+import com.itmo.microservices.demo.basket.impl.repository.BasketEntityRepository
+import com.itmo.microservices.demo.common.exception.NotFoundException
+import com.itmo.microservices.demo.users.impl.repository.UserRepository
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 @RequestMapping("/orders")
-class BasketController(private val basketService: BasketService) {
+class BasketController(
+        private val basketService: BasketService,
+        private val basketEntityRepository: BasketEntityRepository,
+        private val userRepository: UserRepository
+) {
     @GetMapping
     @Operation(
         summary = "Get catalog item list from basket",
@@ -55,7 +65,10 @@ class BasketController(private val basketService: BasketService) {
         @PathVariable(name = "order_id") orderId: UUID,
         @PathVariable(name = "item_id") itemId: UUID,
         @RequestParam amount: Int
-    ) = Unit
+    ) {
+        val details = (SecurityContextHolder.getContext().authentication as UsernamePasswordAuthenticationToken).principal as UserDetails
+        basketService.addCatalogItemToBasket(details, itemId, amount)
+    }
 
     @PostMapping("/{order_id}/bookings")
     @Operation(summary = "Add catalog item to basket",
